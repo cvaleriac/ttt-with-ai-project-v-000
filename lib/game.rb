c5a2d61dc4e43cc4cba8112f786e85737e1f6fb5
valeria campilongo <cvaleriac@hotmail.it>
2018-08-29 00:33:20 +0000

    automatically backed up by learn

---
+++ lib/game.rb
@@ -0,0 +1,18 @@
+class Game
+  @player_1 = player_1
+    @player_2 = player_2
+    @board = board
+    @board.display
+  end
+
+  def turn
+    puts "Please enter a number 1-9:"
+    @user_input = current_player.move(@board)
+    if @board.valid_move?(@user_input)
+      @board.update(@user_input, current_player)
+    else puts "Please enter a number 1-9:"
+      @board.display
+      turn
+    end
+    @board.display
+  end
